c5dd2114080c320adbd0f5e0b113add10f0144fb
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2023-10-22 23:24:08 +0530

    Student Table Created

---
+++ Sql Files/student.sql
@@ -0,0 +1,10 @@
+create table student(
+     StudentID varchar(8) PRIMARY KEY,
+     FirstName varchar(10),
+     LastName varchar(10),
+     NIC varchar(13),
+     Status varchar(15),
+     Address varchar(30),
+     Email varchar(20),
+     DateOfBirth date,
+     Gender char(1));
